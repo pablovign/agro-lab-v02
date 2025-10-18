323f7d72ea3d91f5da8fc1c2ecc47a30a2601b35
Pablo Vignoni <pablovignoni@gmail.com>
2025-10-17 22:24:00 -0300

    commmit inicial

---
+++ src/main/java/com/G6/agro_lab_v02/AgroLabV02Application.java
@@ -0,0 +1,13 @@
+package com.G6.agro_lab_v02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroLabV02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroLabV02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/G6/agro_lab_v02/AgroLabV02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G6.agro_lab_v02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroLabV02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
