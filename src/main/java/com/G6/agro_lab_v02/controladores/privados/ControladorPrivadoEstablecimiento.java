217d63690aa38beaf9a3679abd61d6096407cd48
Pablo Vignoni <pablovignoni@gmail.com>
2025-10-27 20:10:40 -0300

    revisión

--- src/main/java/com/G6/agro_lab_v02/controladores/privados/ControladorPrivadoEstablecimiento.java
+++ src/main/java/com/G6/agro_lab_v02/controladores/privados/ControladorPrivadoEstablecimiento.java
@@ -8,10 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/privado/establecimientos")
@@ -32,5 +31,12 @@ public class ControladorPrivadoEstablecimiento {
         EstablecimientoRespuestaDTO respuesta = servicioEstablecimiento.registrarEstablecimiento(dto, cuit);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(respuesta);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EstablecimientoRespuestaDTO>> listarEstablecimientos(Authentication authentication) {
+        String cuit = authentication.getName();
+        List<EstablecimientoRespuestaDTO> respuesta = servicioEstablecimiento.listarPorEmpresa(cuit);
+        return ResponseEntity.ok(respuesta);
     }
 }
